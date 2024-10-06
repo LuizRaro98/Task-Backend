534f1b73de4bc78cc2128aca6012c6836b8fed7d
LuizRaro98 <brazluiz1000@gmail.com>
2024-10-06 14:33:56 -0300

    Update passport.js to use environment variables

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const { authSecret } = require('../.env')
+const authSecret = process.env.AUTH_SECRET; // Acesse a variável de ambiente diretamente
 const passport = require('passport')
 const passportJwt = require('passport-jwt')
 const { Strategy, ExtractJwt } = passportJwt
